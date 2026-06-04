2459a152ef7ea1828afbe99984802c21759ba946
João <joaocouto.jar@gmail.com>
2026-06-04 01:05:29 -0400

    add Anais 2025

--- src/config/annals.ts
+++ src/config/annals.ts
@@ -10,4 +10,10 @@ export const ANNALS_BY_YEAR: Record<string, Annal[]> = {
       url: "/editais/2024/Anais - I Encontro Científico - Direito UEMS Aquidauana.pdf",
     },
   ],
+  "2025": [
+    {
+      title: "Anais do II Encontro Científico do Curso de Direito",
+      url: "/editais/2025/Anais - II Encontro Científico - Direito UEMS Aquidauana.pdf",
+    },
+  ],
 };
